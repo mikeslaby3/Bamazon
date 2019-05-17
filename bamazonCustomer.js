bb061933c1717a14886895f187bb17c8fa4d4126
Mike Slaby <mikeslaby3@gmail.com>
2019-05-17 16:39:42 -0700

    Create connection to mySQL database

---
+++ bamazonCustomer.js
@@ -0,0 +1,24 @@
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+
+let connection = mysql.createConnection({
+    host: 'localhost',
+    port: 8889,
+    user: 'root',
+    password: 'root',
+    database: 'bamazon'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    afterConnection();
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+  }

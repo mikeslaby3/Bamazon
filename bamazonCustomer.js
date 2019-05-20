90ed3da0edd5b6cec40edaa42022f4fc183ce774
Mike Slaby <mikeslaby3@gmail.com>
2019-05-19 18:19:53 -0700

    Initial logic for purchasing products

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,23 +9,77 @@ let connection = mysql.createConnection({
     database: 'bamazon'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
-    start();
+    afterConnection();
 });
 
-function start() {
-    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
-      if (err) throw err;
-      
-      for (let i = 0; i < res.length; i++) {
-          console.log('Item ID: ' + res[i].item_id);
-          console.log('Product: ' + res[i].product_name);
-          console.log('Price: $' + res[i].price);
-          console.log('\n');
-      }
+function afterConnection() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
 
-    //   connection.end();
+        console.log('\n');
+        console.log('Available Products:');
+        console.log('\n');
+
+        for (let i = 0; i < res.length; i++) {
+            console.log('Item ID: ' + res[i].item_id);
+            console.log('Product: ' + res[i].product_name);
+            console.log('Price: $' + res[i].price);
+            console.log('\n');
+        }
+        //   connection.end();
+        inquirer
+            .prompt([
+                {
+                  name: "item_id",
+                  type: "input",
+                  message: "What's the item ID of the product you'd like to purchase?"
+                },
+                {
+                  name: 'stock_quantity',
+                  type: 'input',
+                  message: 'How many units of product would you like to buy?'
+                }
+            ])
+            .then(function(answer) {
+                connection.query(
+                    'SELECT * FROM products WHERE item_id =' + answer.item_id, 
+                function (err, res){
+                    if (err) {
+                        console.log('Item selection failed');
+                        throw err;
+                    } else {
+                        console.log(res.length);
+                        if (res.length === 0) {
+                            console.log('Item not found');
+                        } else {
+                            if (res[0].stock_quantity >= answer.stock_quantity) {
+                                let updatedQuantity = res[0].stock_quantity - answer.stock_quantity;
+                                connection.query(
+                                  'UPDATE products SET ? WHERE ?', 
+                                  [
+                                    {stock_quantity: updatedQuantity},
+                                    {item_id: answer.item_id}
+                                  ],
+                                  function(err) {
+                                    if (err) {
+                                        console.log('Product update failed');
+                                        throw err;
+                                    } else {
+                                        console.log(answer.stock_quantity + ' item(s) purchased')
+                                    }
+                                  }
+                                );
+                            } else {
+                                console.log('Error: Requested ' + answer.stock_quantity + ' and available = ' + res[0].stock_quantity);
+                            }
+                        }
+                    }
+                    afterConnection();
+                }
+                )
+            });
     });
 }
 

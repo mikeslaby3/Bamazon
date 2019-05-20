452b3472b3b55d776ff87869cba7991c6d044a4f
Mike Slaby <mikeslaby3@gmail.com>
2019-05-19 21:05:07 -0700

    View Low Inventory function displays products with a stock quantity of 5 or below

--- bamazonManager.js
+++ bamazonManager.js
@@ -16,52 +16,76 @@ connection.connect(function (err) {
 
 function afterConnection() {
     inquirer
-    .prompt([
-        {
-            name: "manager_options",
-            type: "list",
-            message: "What would you like to do?",
-            choices: [
-                "View Products for Sale",
-                "View Low Inventory",
-                "Add to Inventory",
-                "Add New Product"
-            ]
-        }
-    ])
-    .then(function (answer) {
-        let choice = answer.manager_options
-        if (choice === "View Products for Sale") {
-            viewProductsForSale();
-        } else if (choice === "View Low Inventory") {
-            viewLowInventory();
-        } else if (choice === "Add to Inventory") {
-            addToInventory();
-        } else if (choice === "Add New Product") {
-            addNewProduct();
-        }
-    });
+        .prompt([
+            {
+                name: "manager_options",
+                type: "list",
+                message: "What would you like to do?",
+                choices: [
+                    "View Products for Sale",
+                    "View Low Inventory",
+                    "Add to Inventory",
+                    "Add New Product",
+                    "Exit"
+                ]
+            }
+        ])
+        .then(function (answer) {
+            let choice = answer.manager_options
+            if (choice === "View Products for Sale") {
+                viewProductsForSale();
+            } else if (choice === "View Low Inventory") {
+                viewLowInventory();
+            } else if (choice === "Add to Inventory") {
+                addToInventory();
+            } else if (choice === "Add New Product") {
+                addNewProduct();
+            } else if (choice === 'Exit') {
+                connection.end();
+            }
+        });
 }
 
 function viewProductsForSale() {
     connection.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-
-        console.log('\n');
-        console.log('Available Products For Sale:');
-        console.log('\n');
-
-        for (let i = 0; i < res.length; i++) {
-            console.log('Item ID: ' + res[i].item_id);
-            console.log('Product: ' + res[i].product_name);
-            console.log('Price: $' + res[i].price);
+        if (err) {
+            console.log('Item list not found');
+            throw err;
+        } else {
             console.log('\n');
+            console.log('Available Products For Sale:');
+            console.log('\n');
+
+            for (let i = 0; i < res.length; i++) {
+                console.log('Item ID: ' + res[i].item_id);
+                console.log('Product: ' + res[i].product_name);
+                console.log('Price: $' + res[i].price);
+                console.log('\n');
+            }
         }
     });
+    connection.end();
 }
 
 function viewLowInventory() {
+    connection.query("SELECT * FROM products WHERE stock_quantity <= 5", function (err, res) {
+        if (err) {
+            console.log('Item list not found');
+            throw err
+        } else {
+            console.log('\n');
+            console.log('Products with Low Inventory:');
+            console.log('\n');
 
+            for (let i = 0; i < res.length; i++) {
+                console.log('Item ID: ' + res[i].item_id);
+                console.log('Product: ' + res[i].product_name);
+                console.log('Price: $' + res[i].price);
+                console.log('\n');
+            }
+        }
+    });
+    connection.end();
 }
 
 function addToInventory() {
@@ -69,5 +93,5 @@ function addToInventory() {
 }
 
 function addNewProduct() {
-    
+
 }
